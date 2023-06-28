83bb4a10ac755cb13e7e981abb2df17a65057bea
Maryam <mariana.alizade@gmail.com>
2023-06-28 10:52:42 +0200

    Fact function is recursive

--- fact.m
+++ fact.m
@@ -1,6 +1,10 @@
  %% FACT compute the factorial of a number
-% FACT(N) computes the factorial of the number N and returns the results 
+% FACT(N) computes the factorial of the number N and returns the results
+% recursively
 function f = fact(n)
-    f = prod(1:n);
+    if n > 1
+        f = n*fact(n-1);
+    else
+        f = n;
+    end
 end
-
